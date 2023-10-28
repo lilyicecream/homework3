b319261ce3141b4bf515287d2a41854808616df6
Darling Rodriguez <drodriguez04@wesleyan.edu>
2023-10-28 17:08:22 -0400

    added a way to switch between forms and made it possible to display items

--- View/App.js
+++ View/App.js
@@ -1,12 +1,43 @@
 import './App.css';
 import React, { useState } from "react";
 import DataDisplay from './DataDisplay';
+import Register from './Registration';
+import Login from './Login';
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
+  const handleRegister = () => {
+    setLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+  }
 
   return (
     <div className="App">
-      <DataDisplay />
+      {loggedIn ? (
+        <>
+          <DataDisplay />
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      ) : (
+        currentForm === "login" ? (
+          <Login onFormSwitch={toggleForm} onLogin={handleLogin} />
+        ) : (
+          <Register onFormSwitch={toggleForm} onRegister={handleRegister} />
+        )
+      )}
     </div>
   );
 }
