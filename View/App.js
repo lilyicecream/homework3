69b5927e181d664bdfdd3d1fb7875bbefdb0b70b
Darling Rodriguez <drodriguez04@wesleyan.edu>
2023-10-28 15:49:21 -0400

    Added react files

---
+++ View/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React, { useState } from "react";
+import DataDisplay from './DataDisplay';
+
+function App() {
+
+  return (
+    <div className="App">
+      <DataDisplay />
+    </div>
+  );
+}
+
+export default App;
