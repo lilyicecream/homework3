c19923728ed1cfc5a7363cc1d0835235c88afe63
lilyicecream <ledelman@wesleyan.edu>
2023-10-30 17:33:12 -0400

    issue7

--- View/DataDisplay.js
+++ View/DataDisplay.js
@@ -24,13 +24,36 @@ const DataDisplay = () => {
                 <p style={{ float: "left" }}>You are logged in as user: <b>{user}</b></p>
             </div>
             <h2>Data from API</h2>
-            <ul>
-                {data?.map((item, index) => (
-                    <li key={index}>{item.username}</li>
-                ))}
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Song Name</th>
+                        <th>Artist</th>
+                        <th>Rating</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data?.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.songName}</td>
+                            <td>{item.artist}</td>
+                            <td>
+                                {/* Display stars here (not functional) */}
+                                {Array(item.rating).fill('⭐️').join(' ')}
+                            </td>
+                            <td>
+                                <button>Update</button>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
+
+
 
 export default DataDisplay;
