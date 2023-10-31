6406356994d95c2305c8d6c9dc8bd97686b40296
lilyicecream <ledelman@wesleyan.edu>
2023-10-30 22:14:54 -0400

    worked on issue 9

--- View/UpdateComponent.js
+++ View/UpdateComponent.js
@@ -1,20 +1,22 @@
 import React, { useState } from 'react';
 
 const UpdateComponent = ({ songDetails, updateSong }) => {
+    const [updatedSong, setUpdatedSong] = useState(songDetails.song);
 
-    const [song, setSong] = useState(songDetails.song);
+    const handleUpdate = () => {
+        updateSong(songDetails.id, updatedSong);
+    }
 
-    const onUpdate = () => {
-        updateSong(songDetails.id, song);
-    }
-
-    return (
-        <div>
-            <label for="title">Title:</label>
-            <input value={song} onChange={e => setSong(e.target.value)} />
-            <button onClick={onUpdate}>Submit</button>
-        </div>
-    )
+    return (
+        <div>
+            <input
+                type="text"
+                value={updatedSong}
+                onChange={(e) => setUpdatedSong(e.target.value)}
+            />
+            <button onClick={handleUpdate}>Update</button>
+        </div>
+    );
 }
 
 export default UpdateComponent;
