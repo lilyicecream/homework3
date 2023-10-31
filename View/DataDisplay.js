69771144367f0f36983612378a4e8119cddbeb67
Darling Rodriguez <drodriguez04@wesleyan.edu>
2023-10-31 19:22:54 -0400

    Fixed and polished the functionatily of the update and delete buttons

--- View/DataDisplay.js
+++ View/DataDisplay.js
@@ -5,99 +5,111 @@ import UpdateComponent from './UpdateComponent';
 import DeleteComponent from './DeleteComponent';
 
 const DataDisplay = () => {
-    const [data, setData] = useState([]);
-    const { user } = useUser();
+    const [data, setData] = useState([]);
+    const { user } = useUser();
 
-    const [mode, setMode] = useState('display'); // Can be 'display', 'update', or 'delete'
-    const [selectedSong, setSelectedSong] = useState(null); // Selected song to be updated or deleted
+    const [mode, setMode] = useState('display'); // Can be 'display', 'update', or 'delete'
+    const [selectedSong, setSelectedSong] = useState(null); // Selected song to be updated or deleted
 
-    const updateSong = async (songId, songUpdate) => {
-        await axios.post('http://localhost/homework3/Controller/RestApi/Ratings/update.php/${song}', songUpdate);
-        const updatedData = [...data];
-        const updatedSongIndex = updatedData.findIndex(item => item.id === songId);
-        updatedData[updatedSongIndex] = { ...updatedData[updatedSongIndex], song: songUpdate };
-        setData(updatedData);
-        setMode('display');
-    }
+    const updateSong = async (songId, songUpdate) => {
+        await axios.post(`http://localhost/homework3/Controller/RestApi/Ratings/update.php`, {
+            id: songId,
+            username: user,
+            artist: songUpdate.artist,
+            song: songUpdate.song,
+            rating: songUpdate.rating,
+        });
+        const updatedData = [...data];
+        const updatedSongIndex = updatedData.findIndex(item => item.id === songId);
+        updatedData[updatedSongIndex] = { ...songUpdate, id: songId, username: user }; // include id and username
+        setData(updatedData);
+        setMode('display');
 
-    const deleteSong = async (song) => {
-        await axios.delete(`http://localhost/homework3/Controller/RestApi/Ratings/delete.php?song=${song}`);
-        setData(data.filter(item => item.song !== song));
-        setMode('display');
-    }
+        // const updatedData = [...data];
+        // const updatedSongIndex = updatedData.findIndex(item => item.id === songId);
+        // updatedData[updatedSongIndex] = songUpdate;
+        // setData(updatedData);
+        // setMode('display');
+    }
+
+    const deleteSong = async (song) => {
+        await axios.delete(`http://localhost/homework3/Controller/RestApi/Ratings/delete.php?song=${song}`);
+        setData(data.filter(item => item.song !== song));
+        setMode('display');
+    }
 
 
-    const handleUpdate = (song) => {
-        setSelectedSong(song);
-        setMode('update');
-    }
+    const handleUpdate = (song) => {
+        setSelectedSong(song);
+        setMode('update');
+    }
 
-    const handleDelete = (song) => {
-        setSelectedSong(song);
-        setMode('delete');
-    }
+    const handleDelete = (song) => {
+        setSelectedSong(song);
+        setMode('delete');
+    }
 
 
-    useEffect(() => {
-        axios.get('http://localhost/homework3/Controller/RestApi/Ratings/Read_ratings.php')
-            .then((response) => {
-                console.log(response.data);
-                setData(response.data.body || []);
-            })
-            .catch((error) => {
-                console.error('Error fetching data:', error);
-                setData([]);
-            });
+    useEffect(() => {
+        axios.get('http://localhost/homework3/Controller/RestApi/Ratings/Read_ratings.php')
+            .then((response) => {
+                console.log(response.data);
+                setData(response.data.body || []);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+                setData([]);
+            });
 
-    }, []);
+    }, []);
 
-    return (
-        <div>
-            <div className="upper-cont">
-                <p style={{ float: "left" }}>You are logged in as user: <b>{user}</b></p>
-            </div>
-            <h2>Data from API</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Username</th>
-                        <th>Song</th>
-                        <th>Artist</th>
-                        <th>Rating</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {mode === 'display' ?
-                        data?.map((item, index) => (
-                            <tr key={index}>
-                                <td>{item.id}</td>
-                                <td>{item.username}</td>
-                                <td>{item.song}</td>
-                                <td>{item.artist}</td>
-                                <td>
-                                    {Array.from({ length: item.rating }).map((_, i) => '⭐️')}
-                                </td>
-                                <td>
-                                    {user === item.username &&
-                                        <>
-                                            <button onClick={() => handleUpdate(item)}>Update</button>
-                                            <button onClick={() => handleDelete(item)}>Delete</button>
-                                        </>
-                                    }
-                                </td>
-                            </tr>
-                        ))
-                        : mode === 'update' ?
-                            <tr><UpdateComponent songDetails={selectedSong} updateSong={updateSong} /></tr>
-                            :
-                            <tr><DeleteComponent songDetails={selectedSong} deleteSong={deleteSong} /></tr>
-                    }
-                </tbody>
-            </table>
-        </div>
-    );
+    return (
+        <div>
+            <div className="upper-cont">
+                <p style={{ float: "left" }}>You are logged in as user: <b>{user}</b></p>
+            </div>
+            <h2>Data from API</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Username</th>
+                        <th>Song</th>
+                        <th>Artist</th>
+                        <th>Rating</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {mode === 'display' ?
+                        data?.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.id}</td>
+                                <td>{item.username}</td>
+                                <td>{item.song}</td>
+                                <td>{item.artist}</td>
+                                <td>
+                                    {Array.from({ length: item.rating }).map((_, i) => '⭐️')}
+                                </td>
+                                <td>
+                                    {user === item.username &&
+                                        <>
+                                            <button onClick={() => handleUpdate(item)}>Update</button>
+                                            <button onClick={() => handleDelete(item)}>Delete</button>
+                                        </>
+                                    }
+                                </td>
+                            </tr>
+                        ))
+                        : mode === 'update' ?
+                            <tr><UpdateComponent songDetails={selectedSong} updateSong={updateSong} /></tr>
+                            :
+                            <tr><DeleteComponent songDetails={selectedSong} deleteSong={deleteSong} /></tr>
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 
--- View/UpdateComponent.js
+++ View/UpdateComponent.js
@@ -2,18 +2,18 @@ import React, { useState } from 'react';
 
 const UpdateComponent = ({ songDetails, updateSong }) => {
     const [updatedSong, setUpdatedSong] = useState(songDetails.song);
+    const [updatedArtist, setUpdatedArtist] = useState(songDetails.artist);
+    const [updatedRating, setUpdatedRating] = useState(songDetails.rating);
 
     const handleUpdate = () => {
-        updateSong(songDetails.id, updatedSong);
+        updateSong(songDetails.id, { song: updatedSong, artist: updatedArtist, rating: updatedRating });
     }
 
     return (
         <div>
-            <input
-                type="text"
-                value={updatedSong}
-                onChange={(e) => setUpdatedSong(e.target.value)}
-            />
+            <input type="text" value={updatedSong} onChange={(e) => setUpdatedSong(e.target.value)} />
+            <input type="text" value={updatedArtist} onChange={(e) => setUpdatedArtist(e.target.value)} />
+            <input type="text" value={updatedRating} onChange={(e) => setUpdatedRating(e.target.value)} />
             <button onClick={handleUpdate}>Update</button>
         </div>
     );
