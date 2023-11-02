9815c5b91e45e07cd01b20011e0a82e867ea62e2
Darling Rodriguez <drodriguez04@wesleyan.edu>
2023-11-01 22:09:09 -0400

    changes

--- View/App.js
+++ View/App.js
@@ -1,15 +1,17 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import DataDisplay from './DataDisplay';
 import Register from './Registration';
 import SongRatingForm from './SongRatingForm';
 import Login from './Login';
 
+
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
   const [loggedIn, setLoggedIn] = useState(false);
   const [songs, setSongs] = useState([]);
+
 
   const toggleForm = (formName) => {
     setCurrentForm(formName);
@@ -27,34 +29,31 @@ function App() {
     setLoggedIn(false);
   }
 
+  // Initial data fetching when component mounts
   useEffect(() => {
     axios.get(`http://localhost/homework3/Controller/RestApi/Ratings/Read_ratings.php`)
       .then(({ data }) => setSongs(data.body || []))
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
+
   return (
     <div className="App">
-      <header>
-        <h1 className="title">Hardcore Music App</h1>
-      </header>
-      <main>
-        {loggedIn ? (
-          <div className="data-and-form">
-            <DataDisplay songs={songs} setSongs={setSongs} />
-            <SongRatingForm setSongs={setSongs} songs={songs} />
-            <button onClick={handleLogout} className="button logout-button">Logout</button>
-          </div>
+
+      <h1 className="title">Hardcore Music App</h1>
+      {loggedIn ? (
+        <div className="data-and-form">
+          <DataDisplay songs={songs} setSongs={setSongs} />
+          <SongRatingForm setSongs={setSongs} songs={songs} />
+          <button onClick={handleLogout} className="button logout-button">Logout</button>
+        </div>
+      ) : (
+        currentForm === "login" ? (
+          <Login onFormSwitch={toggleForm} onLogin={handleLogin} />
         ) : (
-          <div className="auth-form">
-            {currentForm === "login" ? (
-              <Login onFormSwitch={toggleForm} onLogin={handleLogin} />
-            ) : (
-              <Register onFormSwitch={toggleForm} onRegister={handleRegister} />
-            )}
-          </div>
-        )}
-      </main>
+          <Register onFormSwitch={toggleForm} onRegister={handleRegister} />
+        )
+      )}
     </div>
   );
 }
--- View/Registration.jsx
+++ View/Registration.jsx
@@ -1,50 +1,30 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useUser } from './UserContext';
 
-const Register = (props) => {
-  const containerStyle = {
-    border: '10px solid #ff0000',
-    padding: '20px',
-    textAlign: 'center',
-    fontFamily: 'Times New Roman, Times, serif',
-  };
-
-  const titleStyle = {
-    fontSize: '2rem',
-  };
-
-  const inputStyle = {
-    fontFamily: 'Times New Roman, Times, serif',
-  };
-
-  const buttonStyle = {
-    backgroundColor: '#ff0000',
-    color: '#fff',
-    fontFamily: 'Times New Roman, Times, serif',
-    padding: '10px 20px',
-    border: 'none',
-    cursor: 'pointer',
-    margin: '10px 0', // Add vertical space above and below buttons
-  };
+export const Register = (props) => {
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [rePassword, setRePassword] = useState('');
   const [error, setError] = useState('');
+  const { setUser } = useUser();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    // Check if any field is empty
     if (username === '' || password === '' || rePassword === '') {
       setError("All fields are required");
       return;
     }
 
+    // Check if password length is less than 10
     if (password.length < 10) {
       setError("Password must be at least 10 characters long");
       return;
     }
-
+    // Check if passwords match
     if (password !== rePassword) {
       setError("Password doesn't match");
       return;
@@ -52,15 +32,18 @@ const Register = (props) => {
 
     try {
       const response = await axios.post(
-        'http://localhost/homework_3/Controller/RestApi/Register/register.php',
+        'http://localhost/homework3/Controller/RestApi/Register/register.php',
         {
           username,
           password,
-          register: 'register',
+          register: 'register', // Add a parameter to indicate register action
         }
       );
 
+      console.log(response);
+
       if (response.data.message === 'Register successful') {
+        setUser(username);
         console.log('Register successful');
         props.onRegister();
       } else {
@@ -70,28 +53,35 @@ const Register = (props) => {
       console.error('Error sending registration request:', error);
       setError('An error occurred');
     }
-  };
+  }
 
   return (
-    <div style={containerStyle} className="login-container">
-      <h2 style={titleStyle}>Register</h2>
+    <>
       <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label htmlFor="username">Enter Username:</label>
-          <input type="text" name="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} style={inputStyle} />
-          <label htmlFor="password">Enter Password:</label>
-          <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} style={inputStyle} />
-          <label htmlFor="rePassword">Repeat Password:</label>
-          <input type="password" name="rePassword" id="rePassword" value={rePassword} onChange={(e) => setRePassword(e.target.value)} style={inputStyle} />
+        <div className="form-group">
+          <label htmlFor="username">Enter Username:
+            <input type="text" name="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Enter Password:
+            <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="rePassword">Repeat Password:
+            <input type="password" name="rePassword" id="rePassword" value={rePassword} onChange={(e) => setRePassword(e.target.value)} />
+          </label>
         </div>
         <div className="form-btn">
-          <button className="button" type="submit" style={buttonStyle}>Register</button>
+          <input type="submit" value="Register" name="register" />
         </div>
       </form>
-      {error && <div className="error" style={inputStyle}>{error}</div>}
-      <button className="button" onClick={() => props.onFormSwitch('login')} style={buttonStyle}>Already have an account? Login here</button>
-    </div>
+      {error && <div className="error">{error}</div>}
+      <button onClick={() => props.onFormSwitch('login')}>Already have an account? Login here</button>
+    </>
   );
+
 };
 
 export default Register;
